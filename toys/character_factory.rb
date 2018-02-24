2bd556f2a05b48b044601a0a37e64e74d88b5db6
Leam Hall <leamhall@gmail.com>
2018-02-24 11:30:37 -0500

    Working draft of character_factory out for review.

--- toys/character_factory.rb
+++ toys/character_factory.rb
@@ -1,31 +1,45 @@
+class Crew
+  def initialize(crew_number)
+    @crew = []
+    crew_number.times do 
+      ncp     = Character.new 
+      career  = ["Merchant", "Navy"].sample
+      terms   = rand(1..3)
+      new_crewperson(career, terms, ncp)
+      @crew << ncp
+    end
+  end
+  def show_crew
+    counter = 1
+    @crew.each do |c|
+      print("Crewperson #%d is %d years old. Former %s, UPP: %s.\n" %
+        [counter, c.age, c.career, c.upp]) 
+      counter += 1
+    end
+  end
+  def new_crewperson(career,terms,ncp)
+    if career == "Merchant"
+      Merchant.new(ncp, terms)
+    elsif career == "Navy"
+      Navy.new(ncp, terms)
+    end
+  end    
+end
+       
 class Character
   attr_accessor  :age, :career, :name, :upp
+  def initialize
+    @age = 14
+  end
 end
 
 class Career
-  def initialize(char = nil, terms = 1)
-    @char   ||= gen_char(char)
+  def initialize(char, terms = 1)
+    @char   = char
     @terms  = terms
     run_career
   end
-  def gen_char(char)
-    puts "In gen_char char.class is #{char.class}."
-    if char
-      puts "In if char"
-      @char = char
-      #puts @char.age.class
-      #puts @char.age
-    else
-      puts "In else char"
-      @char = Character.new
-      @char.name  = "Ian"
-      @char.age   = 12
-      #puts @char.age.class
-    end
-  end
   def run_career
-    puts "in run_career, char.age is #{@char.age}."
-    puts "terms.class is #{@terms.class}."
     @char.age += @terms * 4
   end
 end
@@ -38,7 +52,7 @@ class Merchant < Career
   end
 end
 
-class Citizen < Career
+class Navy < Career
   def run_career
     super()
     @char.upp = "777777"
@@ -46,17 +60,6 @@ class Citizen < Career
   end
 end
 
-fred = Character.new
-fred.age = 18
-puts fred.age
-
-Citizen.new(fred, 2)
-puts fred.age
-puts fred.upp
-puts fred.career
-puts fred.career.class
-
-joe = Merchant.new
-puts joe.age
-puts joe.upp
-puts joe.career
+### Actually do stuff
+my_crew = Crew.new(6)
+my_crew.show_crew
